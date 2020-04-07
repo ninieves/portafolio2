87326c46e7d959bec494d396a77a82df589c2182
ninieves <ninive.olivaresf@gmail.com>
2020-04-07 16:02:32 -0300

    borre js

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -8,19 +8,7 @@ var actPosition = $("#tile-1 .nav-tabs .active").position();
 $("#tile-1 .slider").css({"left":+ actPosition.left,"width": actWidth});
 
 
-function openCity(evt, cityName) {
-  var i, x, tablinks;
-  x = document.getElementsByClassName("city");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < x.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.firstElementChild.className += " w3-border-red";
-}
+
 
 
 
